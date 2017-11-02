70f8797eb17bae687f87f064f8ce7f264e3c0e3a
D <DarkDrgn2k@networkedserver.net>
2017-11-02 14:39:51 -0400

    Initial working model

---
+++ cjdnsDump.php
@@ -0,0 +1,104 @@
+<?php
+$depth=0;
+function toIP($pub) {
+return trim(shell_exec("/opt/cjdns/publictoip6 " . $pub));
+}
+
+$bl['ksdkzmw2uryvkfg187kxmkdup8k78urqzpn29zh1nxvl6wfdbjk0.k']=
+$bl['3wc3p4dsx6t2ykvp6xwsrqghxpd1sk925llvkvhtvnljdpd6zw30.k']=
+$bl['s51m64dvhm38jlsc5vsjy9r3nry8qzcgpt5t10fxkq32fmnycyp0.k']=
+$bl['jg035j9hup776kwz1k4n0bwpggxp1qmts6t715x53g8vutxktzz0.k']=
+$bl['1941p5k8qqvj17vjrkb9z97wscvtgc1vp8pv1huk5120cu42ytt0.k']=
+$bl['2scyvybg4qqms1c5c9nyt50b1cdscxnr6ycpwsxf6pccbmwuynk0.k']=
+$bl['gx73lzns92j88p1umq2rsqn3ccll28cxuy2z0vjxxmxuplf8yxc0.k']=1;
+//$bl['fgz855zyt2h7wfmf5nj5lf9j2fuh0jbrxrxk2yyqlzj1l9my3jz0.k']=1;
+
+
+function isLink ($p1,$p2) {
+        global $links;
+        if ($p1==$p2) return 1; //is Self
+        if (isset($bl[$p1])) return 1; //is From Blacklist
+        if (isset($bl[$p2])) return 1; //is To Blacklis t
+        if (isset($links[$p1][$p2]))  return 1; //is From To list
+        if (isset($links[$p2][$p1]))  return 1; //is To From list
+        return 0;
+
+}
+
+function AddLink($p1,$p2) {
+global $links;
+
+        if (!isLink($p1,$p2)) {
+                $links[$p1][$p2]=1;
+        }
+}
+
+function Process($path,$pubKey) {
+        global $Links,$depth;
+        if (isset($bl[$pubKey])) return;
+
+        $res=shell_exec("./cexec \"RouterModule_getPeers('" . $path  . "')\"");
+
+        $res=json_decode($res);
+        if (isset($res->peers))  {
+                $p=$res->peers;
+                foreach ($p as $pr) {
+
+
+                        $pr2=explode(".",$pr);
+                        $NewPath=$pr2[1] ."." . $pr2[2] . "." . $pr2[3] . "." . $pr2[4]; //Make NEW PATH
+
+                        $ip=$pr2[5] . ".k"; //Make NEW Pub Key
+                        if (!isset($bl[$ip])) {
+
+                                if ($NewPath!="0000.0000.0000.0001") { //dont process if im processing myself
+
+
+                                        if (!isLink($pubKey,$ip)) { //If not linked already
+                                                AddLink($pubKey,$ip); //Add Link
+                                                for ($i=0; $i<$depth; $i++) echo " ";
+                                                echo $NewPath . "--" . $pubKey .  " -> " . $ip . "\n";
+
+
+                                                $depth++;
+                                                $res2=shell_exec("./cexec \"NodeStore_getRouteLabel('". $path. "','" . $NewPath  . "')\"");
+                                                echo "./cexec \"NodeStore_getRouteLabel('". $path. "','" . $NewPath  . "')\"\n";
+                                                $res2=json_decode($res2);
+                                                //print_r($res2);
+                                                if ($res2->error=="none") {
+                                                        $NewPath=$res2->result;
+                                                        Process ($NewPath,$ip); //Starting Point
+                                                }
+                                                $depth--;
+
+                                        }
+                                } else {
+                //                      echo $NewPath . "--" . $pubKey .  " -> " . $ip . "\n";
+                                }
+                        }
+                }
+
+        }
+}
+
+
+Process ('0000.0000.0000.0001',"ksdkzmw2uryvkfg187kxmkdup8k78urqzpn29zh1nxvl6wfdbjk0.k"); //Starting Point
+
+foreach ($links as $k=>$link) {
+
+        $from=trim(shell_exec("/opt/cjdns/publictoip6 " . $k));
+//$from=$k;
+foreach ($link as $k2=>$parent) {
+
+        $to=trim(shell_exec("/opt/cjdns/publictoip6 " . $k2));
+//$to=$k2;
+                $r['from']=$from;
+                $r['to']=$to;
+                $r['pfrom']=$k;
+                $r['pto']=$k2;
+                $n[]=$r;
+                unset($r);
+                }
+}
+file_put_contents("/var/www/html/links.json" , json_encode($n));
+
