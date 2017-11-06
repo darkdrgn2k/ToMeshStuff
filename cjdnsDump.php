e0ce932548000a743ee9109c4c3ce8e6d6c8b72e
Darkdragon <darkdrgn2k@users.noreply.github.com>
2017-11-05 21:29:31 -0500

    Restructured scanning and added more debugging

--- cjdnsDump.php
+++ cjdnsDump.php
@@ -1,8 +1,15 @@
 <?php
 $stack[]="";
-
 chdir("/opt/cjdns/tools");
 $depth=0;
+function expand($ip){
+    $hex = unpack("H*hex", inet_pton($ip));         
+    $ip = substr(preg_replace("/([A-f0-9]{4})/", "$1:", $hex['hex']), 0, -1);
+
+    return $ip;
+}
+
+
 function logs($l,$depth) {
         for ($i=0; $i<$depth; $i++) echo "| ";
         echo "+" . $l;
@@ -10,61 +17,91 @@ function logs($l,$depth) {
 }
 
 function toIP($pub) {
-        return trim(shell_exec("/opt/cjdns/publictoip6 " . $pub));
+        return expand(trim(shell_exec("/opt/cjdns/publictoip6 " . $pub)));
 }
+
 $checkedNode[]=0;
-
-
+$currentPath="0000.0000.0000.0000";
 
 $res= file_get_contents('https://raw.githubusercontent.com/tomeshnet/node-list/master/nodeList.json');
 $nodes=json_decode($res);
 foreach ($nodes as $node) {
-
         if (isset($node->IPV6Address)) {
-                $wl[$node->IPV6Address]=1;
+                $wl[expand($node->IPV6Address)]=1;
         }
 }
 $wl['fc44:b25f:9533:dede:9242:e5b4:3df6:a99c']=1;
 
-function isLink ($p1,$p2) {
-        global $links,$depth;
-        if ($p1==$p2) return 1; //is Self
-        if (isset($links[$p1][$p2]))  return 1; //is From To list
-        if (isset($links[$p2][$p1]))  return 1; //is To From list
-        logs("Adding " . $p1 . " to " . $p2,$depth);
-        return 0;
 
+function isLink ($p1,$p2) {
+        global $links,$depth,$currentPath;
+        if ($p1==$p2) return 1; //Self link skip
+        if (isset($links[$p1][$p2])) return 1; //is From To list
+        if (isset($links[$p2][$p1])) return 1; //is To From list       
+        return 0;
 }
 
 function AddLink($p1,$p2) {
-        global $links;
+        global $links,$currentPath,$depth;
         if (!isLink($p1,$p2)) {
-                $links[$p1][$p2]=1;
+                logs("Adding " . $p1 . " to " . $p2 . " Over " . $currentPath,$depth);
+                $links[$p1][$p2]=$currentPath;
+        } else {
+                logs("Got it already .. Skipping...",$depth);
         }
 }
 
 function ProcessManual($path,$pubKey) {
-        global $Links,$depth,$checkedNode,$checkNodeLinks,$wl,$stack;
+        global $Links,$depth,$checkedNode,$checkNodeLinks,$wl,$stack,$currentPath;
         array_push($stack, $path . "." . $pubKey);
+        $currentPath=$path;
+        logs("Begin Proces of " . $pubKey . "+++" , $depth);
 
-
-        logs("ScanOf " . $pubKey . "+++" , $depth);
-print_r($checkedNode);
         $res=shell_exec("./cexec \"RouterModule_getPeers('" . $path  . "')\"");
-
+        logs("EXEC:  ./cexec \"RouterModule_getPeers('" . $path  . "')\"",$depth);
         $res=json_decode($res);
+
+
+
+$skip=0;
         //First add links
-                if (isset($res->peers))  {
-                $p=$res->peers;
-                foreach ($p as $pr) {
-                        $pr2=explode(".",$pr);
-                        $NewPath=$pr2[1] ."." . $pr2[2] . "." . $pr2[3] . "." . $pr2[4]; //Make NEW PATH
-                        $newPubKey=$pr2[5] . ".k"; //Make NEW Pub Key
-                        addLink($pubKey,$newPubKey);
+
+        if (isset($res->peers))  {
+            $p=$res->peers;
+
+
+                $pr=$p[0];
+                $pr2=explode(".",$pr);
+
+                $newPubKey=$pr2[5] . ".k"; //Make NEW Pub Key
+                $NewPath=$pr2[1] ."." . $pr2[2] . "." . $pr2[3] . "." . $pr2[4]; //Make NEW PATH
+                if ($NewPath=="0000.0000.0000.0001") {
+                        if ($newPubKey  !=  $pubKey) {
+                                logs("*******WRONG NODE - SKUPPING*",$depth);
+print_r($stack);
+$skip=1;
+                        }
+                } else {
+echo $newPubKey . "!=" . $pubKey;
+                        die("WRONG PATH");
                 }
-                //Next Rerusrse
-                $p=$res->peers;
-                foreach ($p as $pr) {
+
+
+logs("---------------------[LINKS]-------------------",$depth);
+if (!$skip) {
+            foreach ($p as $pr) {
+                $pr2=explode(".",$pr);
+                $newPubKey=$pr2[5] . ".k"; //Make NEW Pub Key
+                $NewPath=$pr2[1] ."." . $pr2[2] . "." . $pr2[3] . "." . $pr2[4]; //Make NEW PATH
+                logs("Found $newPubKey over $NewPath",$depth);
+                $res=addLink($pubKey,$newPubKey);
+
+            }
+            unset($newPubKey);
+
+
+            //Next Recusrse
+            foreach ($p as $pr) {
                         $pr2=explode(".",$pr);
                         $NewPath=$pr2[1] ."." . $pr2[2] . "." . $pr2[3] . "." . $pr2[4]; //Make NEW PATH
                         $newPubKey=$pr2[5] . ".k"; //Make NEW Pub Key
@@ -73,8 +110,8 @@ print_r($checkedNode);
                                 $checkedNode[$newPubKey]=1;
                                 //logs ($NewPath . "--" . $pubKey .  " -> " . $ip ,$depth);
                                 $depth++;
-                                $NewPath=trim(shell_exec("./splice $NewPath $path"));
-
+                                $NewPathSplice=trim(shell_exec("./splice $NewPath $path"));
+logs("Exec ./splice $NewPath $path" , $depth);
 /*                                $res2=shell_exec("./cexec \"NodeStore_getRouteLabel('". $path. "','" . $NewPath  . "')\"");
                                 $res2=shell_exec("./cexec \"NodeStore_getRouteLabel('". $path. "','" . $NewPath  . "')\"");
 //                              echo "./cexec \"NodeStore_getRouteLabel('". $path. "','" . $NewPath  . "')\"\n";
@@ -85,7 +122,7 @@ echo " $res3 = " . $res2->result . "\n";
 
                                        $NewPath=$res2->result;
 */
-                                        ProcessManual($NewPath,$newPubKey); //Starting Point
+                                        ProcessManual($NewPathSplice,$newPubKey); //Starting Point
   //                              } else {
 //                                      logs("Error $newPubKey / $path / $NewPath "  . $res2->error, $depth);
 //                              }
@@ -105,15 +142,17 @@ echo " $res3 = " . $res2->result . "\n";
                                 $checkedNode[$newPubKey]=1;
                         }
                 }
+}
         } else  {
-$e=$res->error;
-if (isset($res->result)) $e=" " . $res->result;
+                $e=$res->error;
+                if (isset($res->result)) $e=" " . $res->result;
 
-logs("Failed " . $e . "  $path $pubKey",$depth);
-print_r($stack);
+                logs("Failed " . $e . "  $path $pubKey",$depth);
+                print_r($stack);
         }
 
-                                array_pop($stack);
+
+         array_pop($stack);
 
 }
 
@@ -158,3 +197,4 @@ foreach ($links as $k=>$link) {
 unlink("/var/www/html/links.json");
 file_put_contents("/var/www/html/links.json" , json_encode($n));
 print_r($n);
+
