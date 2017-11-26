3f0fca62e4ca1d514c767ee140178cb35b9ae507
Darkdragon <darkdrgn2k@users.noreply.github.com>
2017-11-26 17:05:56 -0500

    Basic AP setup

--- altConfig/setup.sh
+++ altConfig/setup.sh
@@ -7,6 +7,30 @@ additionalParam="--ascii-lines "
 
 interfaces=$(ls /sys/class/net/)
 
+
+function removeSection()
+{
+res=""
+    while read -r line
+        do
+
+        if [[ "$line" != "$1" ]]; then
+
+                res="$res\n[$line]"
+
+                while read -r line2
+                do
+                        res="$res\n$line2"
+                done < <(confget -f /etc/mesh.conf  -s $line -l)
+
+        fi
+        done < <(cat /etc/mesh.conf  | grep "\[" | awk -F "\[" '{print $2}' | awk -F "\]" '{print $1}')
+        echo -e $res >/etc/mesh.conf
+
+
+}
+
+
 function getMAC()
 {
         if [[  $1 != '' ]]; then
@@ -29,8 +53,6 @@ function MainMenu() {
                         list="$list $interface '$mac $type'"
                 fi
         fi
-
-
 
         done
         eval "dialog --menu 'Select interface to configure' 20 51 19 $list"  2> /tmp/res
@@ -75,7 +97,7 @@ function GetFrequency() {
                 list="$list off"
 
                 fi
-        done < <(iwlist wlan0 frequency | grep Channel | awk '{print $4'})
+        done < <(iwlist $intSelected frequency | grep Channel | awk '{print $4'})
 
         eval "dialog --radiolist 'Frequency' 20 51 10 $list "  2>/tmp/res
         intFreq=$(cat /tmp/res)
@@ -83,22 +105,56 @@ function GetFrequency() {
 
 }
 
+function GetChannel() {
+        intFreq=$(confget -f /etc/mesh.conf -s $mac freq)
+
+        list=""
+        while read -r line
+        do      clean=$(echo $line)
+                list="$list $clean 'Chan $line'"
+                if [[ "$intFreq" == "$clean" ]]; then
+                list="$list on"
+
+                else
+                list="$list off"
+
+                fi
+        done < <(iwlist $intSelected frequency | grep Channel | awk '{print $2'})
+
+        eval "dialog --radiolist 'Channe' 20 51 10 $list "  2>/tmp/res
+        intChan=$(cat /tmp/res)
+
+
+}
+
+echo $list
 MainMenu
 SelectType $intSelected
 GetSSID
 
 
-#if [[ "$intType" == "mesh" ]]; then
-#echo 1
+
+if [ "$intType" == "mesh" ]; then
         GetMeshProtocol
-#fi
+        GetFrequency
+        mac=$(getMAC $intSelected)
+        removeSection $mac
 
-GetFrequency
+        echo \[$mac\] >> /etc/mesh.conf
+        echo type=$intType  >> /etc/mesh.conf
+        echo ssid=$intSSID  >> /etc/mesh.conf
+        echo freq=$intFreq   >> /etc/mesh.conf
+        echo protocol=$intProtocol  >> /etc/mesh.conf
+fi
 
-echo \[$mac\]
-echo type=$intType
-echo ssid=$intSSID
-echo freq=$intFreq
-echo protocol=$intProtocol
+if [ "$intType" == "ap" ]; then
+        mac=$(getMAC $intSelected)
+        GetChannel
+        removeSection $mac
 
+        echo \[$mac\] >> /etc/mesh.conf
+        echo type=$intType  >> /etc/mesh.conf
+        echo ssid=$intSSID  >> /etc/mesh.conf
+        echo chan=$intChan   >> /etc/mesh.conf
 
+fi
