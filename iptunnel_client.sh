b8af3eb0e6c2cafd0d8f26683a0ee189bffcf83c
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2018-01-25 10:57:16 -0500

    Updated server

--- iptunnel_client.sh
+++ iptunnel_client.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # connect to NODE2 IP Tunnel for internet
 key=$(sudo grep -m 1 '"publicKey"' /etc/cjdroute.conf | awk '{ print $2 }' | sed 's/[",]//g')
-curl http://[fc6e:691e:dfaa:b992:a10a:7b49:5a1a:5e09]/test.php?key=$key
-/opt/cjdns/tools/cexec  "IpTunnel_connectTo('ksdkzmw2uryvkfg187kxmkdup8k78urqzpn29zh1nxvl6wfdbjk0.k')"
+curl http://[fc96:47e7:2115:dffa:d406:8765:e260:5a72]/api/api.php?key=$key
+/opt/cjdns/tools/cexec  "IpTunnel_connectTo('kb2j7m6rqv3lyt5xm0zz4g8jfdc2qws1kdf8fb8wyjs2ysqbfr30.k')"
 iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
 iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
